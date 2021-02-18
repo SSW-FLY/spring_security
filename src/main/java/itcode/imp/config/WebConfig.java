f402a9f8eff685a69891751009bd1bc1dc29301d
SSW-FLY <ssw_fly@163.com>
2021-02-18 23:39:17 +0800

    修改

--- src/main/java/itcode/imp/config/WebConfig.java
+++ src/main/java/itcode/imp/config/WebConfig.java
@@ -1,13 +1,15 @@
 package itcode.imp.config;
 
-import javax.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
 
 /**
  * @author imp
@@ -29,5 +31,13 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("")
+                .defaultSuccessUrl("")
+                .and();
     }
 }
