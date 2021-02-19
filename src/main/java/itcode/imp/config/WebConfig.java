dec8e86120b8e18082c830fdda024d5534ae09a7
yangcheng <cheng.yang@mail.hypers.com>
2021-02-19 10:51:43 +0800

    增加

--- src/main/java/itcode/imp/config/WebConfig.java
+++ src/main/java/itcode/imp/config/WebConfig.java
@@ -36,10 +36,16 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage("")
-                .loginProcessingUrl("")
-                .defaultSuccessUrl("")
-                .and()
-                .authorizeRequests();
+            .loginPage("")
+            .loginProcessingUrl("")
+            .defaultSuccessUrl("")
+            .and()
+            .authorizeRequests()
+            .antMatchers("")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .antMatcher("");
     }
 }
