f6b7895030504012ea97d637fae9b1263d956fa2
yangcheng <cheng.yang@mail.hypers.com>
2021-03-08 18:48:27 +0800

    update

---
+++ src/main/java/itcode/imp/entity/PostNode.java
@@ -0,0 +1,20 @@
+package itcode.imp.entity;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author imp
+ * @date 2021/3/8
+ */
+public class PostNode {
+
+    private String nodeType;
+
+    private PostNode node;
+
+    private String operate;
+
+    private PostNode left;
+
+    private PostNode right;
+}
---
+++ src/main/java/itcode/imp/util/PostAnalyzer.java
@@ -0,0 +1,17 @@
+package itcode.imp.util;
+
+import itcode.imp.entity.PostNode;
+
+/**
+ * @author imp
+ * @date 2021/3/8
+ */
+public class PostAnalyzer {
+
+    private final static String RIGHT = "(";
+    private final static String LEGTH = ")";
+
+    public static PostNode createTree(String str) {
+        return new PostNode();
+    }
+}
